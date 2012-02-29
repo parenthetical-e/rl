Finalizing design.  Fix softmax, work out how to override it for behavioral fitting.  Then serious testing.

rlalgorithms/policy.py
@@ -4,32 +4,35 @@ Policies for selecting the next state
 import numpy as np
 import bisect as bi
 
-def softmax(s,Vs,Ss,beta):
-	p_denom = sum([np.exp(beta*V_i) for V_i in Vs])
-	p_Vs = [np.exp(beta*v)/p_denom for V_i in Vs]
-	p_s = 0
-	s_out = None
-	if s is not None:
-		# Do a waeighted sompling of Ss using p_Vs. 
-		# 
-		# Code modified from:
-		# http://eli.thegreenplace.net/2010/01/22/weighted-random
-		# -generation-in-python/ 
-	    totals = []
-	    running_total = 0
-	    for w in p_Vs:
-	        running_total += w
-	        totals.append(running_total)
-		rnd = np.random.rand() * running_total
-		loc = bi.bisect_right(totals, rnd)
-		s_out = Ss[loc]
-		p_s = p_Vs[loc]
-	else:
-		# If s was defined (i.e. not None) just use it.
-		s_out = s.copy()
-		p_s = p_Vs[Ss.index(s)]
+# TODO fix so this uses use only attr and params
+# TODO vectorize
+def softmax(self,beta):
+	""" 
+	Use the current and past v and the logistic function 
+	to guess the state with the highest probability of winning.
+	"""
+	from collections import deepcopy
+	
+	# Find all possible states
+	known_states = list(set(history['s']))
+	known_states.sort()
+	
+	# Get the values for those states
+	latest_v = np.zeros(len(known_states))
+	for i,ks in enumerate(known_states):
+		latest_v[i] = self.get_from_history(self,ks,'v',1)
+	
+	# Get p for each latest_v (vectorized)
+	ps = np.exp(latest_v*beta) / np.sum(np.exp(beta*latest_v))
+	
+	# TODO p weighted sampling of known_states goes here
+	# http://prxq.wordpress.com/2006/04/17/the-alias-method/
+	# for an example of how to do this.  None of pythons libs seem
+	# to support it.
 		
-	return s_out, p_s
+	# Return the state match p_use
+	known_states = np.asarray(known_states)	
+	return known_states[p_used == ps], p_u
 
 
 def e_greedy(Vs,Ss,epsilon):

spaces/rewards.py
@@ -1,14 +1,14 @@
-""" Simulate reward spaces. """
+""" Generate reward spaces. """
 import numpy as np
 import scipy.stats as stats
 
-def random(N,p):
+def random(p,N):
 	"""
 	Yield a single trials worth of random behavioral accuracy. P(1) = p.
 	"""
 
 	reward_space = np.random.binomial(1,p,N)
-	for r in reward_space:
+	for r in reward_space: 
 		yield r
 
 	# state_space = np.array(state_space)
@@ -38,7 +38,6 @@ def learn_logistic(N):
 	learning.
 	"""
 
-	
 	# Simulate learning:
 	# Create a noisy range for the CDF,
 	trials = np.arange(.01,10,10/float(N))
@@ -51,7 +50,7 @@ def learn_logistic(N):
 	p_values = p_tmp
 		
 	for p in p_values:
-		yield int(np.random.binomial(1,p,(1)))
+		yield np.random.binomial(1,p))
 	
 
 